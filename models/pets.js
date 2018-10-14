cd3528d5d8fde61d1da45772c7e2c5dbb0e77116
alyssackieffer <alyssackieffer@Alyssas-MacBook-Pro.local>
2018-10-14 19:52:27 -0400

    CRUD achieved with bugs

---
+++ models/pets.js
@@ -0,0 +1,16 @@
+//Require Mongoose
+const mongoose = require('mongoose');
+//Blueprint
+const petSchema = mongoose.Schema({
+  species: { type: String, required: true },
+  breed: String,
+  img: String,
+  readyToGoHome: Boolean,
+  price: { type: Number, default: 0 },
+  qty: { type: Number, default: 0 },
+  rating: Number
+});
+//Name variable
+const Pet = mongoose.model('Pet', petSchema);
+//Export new addition
+module.exports = Pet;
--- server.js
+++ server.js
@@ -4,13 +4,13 @@ const methodOverride = require('method-override');
 const mongoose = require('mongoose');
 const app = express();
 const db = mongoose.connection;
-
 //Port
 //Allow use of Heroku's port or local Port
 const PORT = process.env.PORT || 3000
-
 //Database
 const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/' + 'stcpetshop'
+//Require petSchema
+const Pet = require('./models/pets.js');
 
 //Connect to Mongo
 mongoose.connect(MONGODB_URI, { useNewUrlParser: true })
@@ -25,7 +25,6 @@ db.on('open', ()=>{});
 
 //Middleware
 //Use public folder for static assests (CSS)
-app.use(express.static('public'));
 
 //Populated req.body with parsed info from forms, if no data, return empty
 app.use(express.urlencoded({ extended: false}));
@@ -35,8 +34,66 @@ app.use(express.urlencoded({ extended: false}));
 app.use(methodOverride('_method'));
 
 //Routes
-app.get('/', (req, res)=>{
-  res.send('Hello world!');
+//Link INDEX
+app.get('/pets', (req, res)=>{
+  Pet.find({}, (err, allPets)=>{
+    res.render('index.ejs', {
+      pets: allPets
+    });
+  });
+});
+
+//Link NEW
+app.get('/pets/new', (req, res)=>{
+  res.render('new.ejs');
+});
+
+//Link SHOW
+app.get('/pets/:id', (req, res)=>{
+  Pet.findById(req.params.id, (err, foundPet)=>{
+    res.render('show.ejs', {
+      pet: foundPet
+    });
+  });
+});
+
+//Link EDIT
+app.get('/pets/:id/edit', (req,res)=>{
+  Pet.findById(req.params.id, (err, foundPet)=>{
+    res.render('edit.ejs', {
+      pet: foundPet
+    });
+  });
+});
+
+//Define PUT
+app.put('/pets/:id', (req, res)=>{
+  if (req.body.readyToGo === 'on'){
+    req.body.readyToGo = true;
+  } else {
+    req.body.readyToGo = false;
+  }
+  Pet.findByIdAndUpdate(req.params.id, req.body, { new: true }, (err, updatePet)=>{
+    res.redirect('/pets/:id');
+  });
+});
+
+//Define POST
+app.post('/pets', (req, res)=>{
+  if (req.body.readyToGo === 'on'){
+    req.body.readyToGo = true;
+  } else {
+  req.body.readyToGo = false;
+}
+///Send created pet to array
+  Pet.create(req.body, (err, createdPet)=>{
+    res.redirect('/pets');
+  });
+});
+
+//Define DELETE
+app.delete('pets/:id', (req, res)=>{
+  res.redirect('/pets');
 });
 
 //Listener
